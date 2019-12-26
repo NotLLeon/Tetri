d8563642d21c0ce83d2c5e7fbd806c7ecf742ace
Leon Liu <leonyi.liu@gmail.com>
2019-12-26 17:45:38 -0500

    game runs, displays red screen

--- Tetri/Tetri.cpp
+++ Tetri/Tetri.cpp
@@ -1,11 +1,14 @@
 // Tetri.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "stdafx.h"
+#include "Game.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Game::Start();
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
